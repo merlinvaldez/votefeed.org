119f3a63ef7c830a6d742c0fb1d3ff63d63d2c63
Merlin Valdez <163349473+merlinvaldez@users.noreply.github.com>
2026-02-14 12:02:20 -0500

    index.js change

--- server/api/index.js
+++ server/api/index.js
@@ -1,11 +1,24 @@
 import app from "../app.js";
 import db from "../src/db/client.js";
 
-let isConnected = false;
+let connectPromise = null;
 
-if (!isConnected) {
-  await db.connect();
-  isConnected = true;
+async function ensureDbConnected() {
+  if (connectPromise) return connectPromise;
+  connectPromise = db.connect().catch((err) => {
+    connectPromise = null;
+    throw err;
+  });
+  return connectPromise;
 }
+
+app.use(async (req, res, next) => {
+  try {
+    await ensureDbConnected();
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
 
 export default app;
